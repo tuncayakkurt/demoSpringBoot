c71425e772f863a5f3d5b84acb67b37eb641b85a
Tuncay AKKURT <takkurt@innova.com.tr>
2022-09-01 18:27:55 +0300

    demoSpringBoot youtube second version

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -17,26 +17,35 @@ public class StudentController {
     }
 
     //GET
+    //http://localhost:8080/api/v1/student
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
     }
 
     //POST
+    //http://localhost:8080/api/v1/student
+    //{
+    //    "name": "tunca",
+    //    "email": "tunca@yy",
+    //    "dob":"1995-12-11"
+    //}
     @PostMapping
     public void registerNewStudent(@RequestBody Student student) {
         studentService.addNewStudent(student);
     }
 
     //DELETE
+    //http://localhost:8080/api/v1/student/1
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Long studentId) {
         studentService.deleteStudent(studentId);
     }
 
     //PUT (update)
-    @Transactional
-    public void updateStudent(Long studentId) {
-        studentService.deleteStudent(studentId);
+    //http://localhost:8080/api/v1/student/1?name=abc&email=abc@mail
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(@PathVariable("studentId") Long studentId, @RequestParam(required = false) String name, @RequestParam(required = false) String email) {
+        studentService.updateStudent(studentId, name, email);
     }
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,8 +3,9 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -37,7 +38,25 @@ public class StudentService {
         studentRepository.deleteById(studentId);
     }
 
-    public void updateStudent(Long studentId) {
-
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        boolean exists = studentRepository.existsById(studentId);
+        if (!exists) {
+            throw new IllegalStateException("student id " + studentId + " does not exist");
+        }
+        if (name != null && name.length() > 0 && !Objects.equals(studentRepository.findById(studentId).get().getName(), name)) {
+            if (studentRepository.findById(studentId).isPresent()) {
+                studentRepository.findById(studentId).get().setName(name);
+            }
+        }
+        if (email != null && email.length() > 0 && !Objects.equals(studentRepository.findById(studentId).get().getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email already exists " + email);
+            }
+            if (studentRepository.findById(studentId).isPresent()) {
+                studentRepository.findById(studentId).get().setEmail(email);
+            }
+        }
     }
 }
